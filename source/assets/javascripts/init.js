4ed11380d3c4e8c34b425a77eb147fa77a41477b
Rory Powis <rory@hikkendry.com>
2014-04-09 10:54:31 +0100

    FIX: contact dropdown JS

--- source/assets/javascripts/init.js
+++ source/assets/javascripts/init.js
@@ -1,31 +1,10 @@
 (function(w, $){
-	var sw = document.body.clientWidth,
-		sh = document.body.clientHeight;
-
-	$(w).resize(function(){ //Update dimensions on resize
-		sw = document.body.clientWidth;
-		sh = document.body.clientHeight;
-		
-		//updateAds();
-	});
-
-
-	//Navigation toggle
-	if ( $(window).width() < 800 ) {
-		$('.pagenav').on('click', '.nav-toggle-menu', function(e) {
-			e.preventDefault();
-			$(this).toggleClass('active');
-			$('.nav').toggleClass('active');
-			$('.header .contact').removeClass('active');
-		});
-		
-		//Navigation toggle
-		$('.pagenav').on('click', '.contact-toggle-menu', function(e) {
-			e.preventDefault();
-			$(this).toggleClass('active');
-			$('.header .contact').toggleClass('active');
-			$('.nav').removeClass('active');
-		});
-	};
+  
+  // Toggle contact
+  var $nav = $('.pagenav');
+  $nav.on('click', '.contact-toggle', function(e) {
+    e.preventDefault();
+    $('.contact.dropdown', $nav).toggleClass('active');
+  });
 
 })(this, jQuery);
