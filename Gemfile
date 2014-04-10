8be6d4f09c543b456b8b8fe45e0c1a2fa89da759
Rory Powis <rory@hikkendry.com>
2014-04-10 16:10:19 +0100

    Add middleman-deploy with config

--- Gemfile
+++ Gemfile
@@ -2,10 +2,14 @@
 # the following line to use "https"
 source 'http://rubygems.org'
 
+# middleman core
 gem "middleman", "~>3.2.2"
+
+# middleman plugins
+gem "middleman-livereload", "~> 3.1.0"
+gem "middleman-deploy", "~>0.2.3"
+
+# SASS plugins
 gem "slim", "~> 2.0.2"
 gem "scut", "~> 0.10.2"
 gem "susy", "~> 1.0.9"
-
-# Live-reloading plugin
-gem "middleman-livereload", "~> 3.1.0"
--- config.rb
+++ config.rb
@@ -7,13 +7,25 @@ require "scut"
 activate :livereload
 activate :directory_indexes
 
-set :build_dir, 'httpdocs'
 set :js_dir, 'assets/javascripts'
 set :css_dir, 'assets/stylesheets'
 set :images_dir, 'assets/images'
 
 ready do
   sprockets.append_path File.join root, 'bower_components'
+end
+
+# Dynamically create project pages
+data.projects.each do |project|
+	proxy "/#{project[:title].parameterize}.html", "/project.html", :locals => { :content => project }, :ignore => true
+end
+
+# Deploy config
+activate :deploy do |deploy|
+  deploy.build_before = true
+  deploy.method = :git
+  deploy.remote = "github"
+  deploy.branch = "master"
 end
 
 # Build-specific configuration
@@ -23,16 +35,11 @@ configure :build do
   # activate :minify_javascript
   activate :asset_hash
 
-	# compass_config do |config|
-	#   config.output_style = :compact
-	# end
+  # compass_config do |config|
+  #   config.output_style = :compact
+  # end
 
   # activate :relative_assets
   # set :relative_links, true
 
-end
-
-# Dynamically create project pages
-data.projects.each do |project|
-	proxy "/#{project[:title].parameterize}.html", "/project.html", :locals => { :content => project }, :ignore => true
 end
