95faa6a9d98a7b7b6316e186c83f901b03489aff
Rory Powis <rory@hikkendry.com>
2014-04-30 15:20:19 +0100

    Minify CSS and JS on build

--- config.rb
+++ config.rb
@@ -30,8 +30,8 @@ end
 
 # Build-specific configuration
 configure :build do
-  # activate :minify_css
-  # activate :minify_javascript
+  activate :minify_css
+  activate :minify_javascript
   activate :asset_hash
 
   activate :imageoptim do |options|
